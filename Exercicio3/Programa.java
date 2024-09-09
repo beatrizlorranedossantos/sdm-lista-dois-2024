3ec8773bc79376d1ed5869d1b7e750ced20e554b
beatrizlorranedossantos <142808873+beatrizlorranedossantos@users.noreply.github.com>
2024-09-09 19:49:40 -0300

    Create Programa.java

---
+++ Exercicio3/Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.acordar();
+        pessoa.andar();
+        pessoa.comer();
+        pessoa.dormir();
+    }
+}
